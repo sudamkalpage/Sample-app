ca412bd5b9e2e243ae3a668940737ec02f3dd02c
sudamkalpage <58980965+sudamkalpage@users.noreply.github.com>
2021-01-13 00:17:03 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,25 @@
+var express = require('express')
+const app = express()
+app.get('/',function(req,res){
+    res.send('Hello Client!')
+})
+app.get('/login',function(req,res){
+    res.send('Login page')
+})
+app.get('/signup',function(req,res){
+    res.send('Sign up page')
+})
+
+app.get('/user:id',function(req,res){
+    const id = req.params.id
+    res.send('Welcome User'+id)
+})
+app.get('/user',function(req,res){
+    const id = req.query.id
+    res.send('Welcome User'+id)
+})
+app.listen(9000,function(req,res){
+    console.log('Running..')
+})
+
+
