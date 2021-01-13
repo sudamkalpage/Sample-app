152743348770a9002d4bb06cc2dcb0cec4421702
sudamkalpage <58980965+sudamkalpage@users.noreply.github.com>
2021-01-13 17:29:32 +0530

    initial pug files

--- app.js
+++ app.js
@@ -1,17 +1,29 @@
 const express = require('express')
-const app = express()
+const path = require('path')
+
+//init app
+const app = express()   
 const port = 9000
 
+//Loadview Engine 
+app.engine('pug', require('pug').__express)
+app.set('views',path.join(__dirname,'views'))
+app.set('view engine','pug')
+
+//home route
 app.get('/',function(req,res){
-    res.send('Hello Client!')
+    res.render('index',{
+        title:'Hello'
+    })
 })
 app.get('/login',function(req,res){
-    res.send('Login page')
+    res.render('index',{
+        title:'Login'
+    })
 })
 app.get('/signup',function(req,res){
     res.send('Sign up page')
-})
-
+}) 
 app.get('/user:id',function(req,res){
     const id = req.params.id
     res.send('Welcome User'+id)
@@ -20,8 +32,8 @@ app.get('/user',function(req,res){
     const id = req.query.id
     res.send('Welcome User'+id)
 })
-app.listen(9000,function(req,res){
-    console.log('Running..')
+
+//start server
+app.listen(port,function(req,res){
+    console.log(`Example app listening at http://localhost:${port}`)
 })
-
-
